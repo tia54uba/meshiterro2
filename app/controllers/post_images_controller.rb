9b1cbd0901d56e8b88ba4369db4df77e4d6af258
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-06-04 14:24:52 +0000

    add create

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,6 +1,13 @@
 class PostImagesController < ApplicationController
   def new
-    @Post_image = PostImage.new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
